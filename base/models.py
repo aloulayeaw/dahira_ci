d0a5e919d2f864f29f246c5777f7be040755b729
aloulayeaw <47934153+aloulayeaw@users.noreply.github.com>
2024-01-08 14:51:06 +0100

    Project Deploy

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    tittle=models.CharField(max_length=255)
+    date=models.CharField( max_length=255)
+    designation=models.CharField(max_length=255, blank=True)
+    photo=models.ImageField( upload_to='photos/%Y/%m/%d/')
+    is_featured = models.BooleanField(default=False, null=True , blank=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tittle
