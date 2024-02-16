bc808abb1dab7032ba4eb7472d10f9bdad178352
aloulayeaw <47934153+aloulayeaw@users.noreply.github.com>
2024-02-16 11:23:05 +0100

    Deplow Web

--- base/urls.py
+++ base/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     #path('pdf_view/<path:pdf_file_path>/', views.pdf_view, name='pdf_view'),
     path('kedougou', views.destinationTwo, name='destinationTwo'),
     path('blog', views.blog , name='blog'),
-<<<<<<< HEAD
     path('blog_detail', views.blog_detail , name='blog_detail'),
-=======
->>>>>>> d0a5e919d2f864f29f246c5777f7be040755b729
     path('details/<str:id>', views.details , name='details'),
 ]
