9d5a3f6354bdafbfcfcf18023cc934c256acd6df
aloulayeaw <47934153+aloulayeaw@users.noreply.github.com>
2024-02-16 11:10:04 +0100

    Deploy App

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('mediatheque', views.mediatheque, name='mediatheque'),
+    path('bibliotheque', views.bibliotheque, name='bibliotheque'),
+    path('contact', views.contact, name='contact'),
+    #path('pdf_view/<path:pdf_file_path>/', views.pdf_view, name='pdf_view'),
+    path('kedougou', views.destinationTwo, name='destinationTwo'),
+    path('blog', views.blog , name='blog'),
+    path('blog_detail', views.blog_detail , name='blog_detail'),
+    path('details/<str:id>', views.details , name='details'),
+]
